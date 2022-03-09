7b84412cbf676b4efae3a5ba3c603b1ef40a67c9
K.K. Pasindu Perakum <99235576+IT21160066@users.noreply.github.com>
2022-03-09 08:09:33 +0000

    tutorial-03

--- exercise02.cpp
+++ exercise02.cpp
@@ -3,31 +3,42 @@ using namespace std;
 
 int volume(int height, int width, int length);
 
+struct box{
+	int Height;
+	int Width;
+	int Length;
+};
+
 int main() {
-    int box1Height, box1Width, box1Length;
-    int box2Height, box2Width, box2Length;
-    int totalVolume, totalSurface;
+	
+	struct box box1, box2;
+    int totalVolume;
     
     cout << "Enter Box 1 Height : ";
-    cin >> box1Height;
+    cin >> box1.Height;
     cout << "Enter Box 1 Width : ";
-    cin >> box1Width;
+    cin >> box1.Width;
     cout << "Enter Box 1 Length : ";
-    cin >> box1Length;
+    cin >> box1.Length;
     
      cout << "Enter Box 2 Height : ";
-    cin >> box2Height;
+    cin >> box2.Height;
     cout << "Enter Box 2 Width : ";
-    cin >> box2Width;
+    cin >> box2.Width;
     cout << "Enter Box 2 Length : ";
-    cin >> box2Length;
+    cin >> box2.Length;
     
     totalVolume = volume(box1Height, box1Width, box1Length)
              + volume(box2Height, box2Width, box2Length);
-             
+
     cout << "Volume of Box is " << totalVolume << endl;
     
     return 0;
 }
 
 // Implement the Volume() function here
+int volume(int height, int width, int length){
+
+  return height * width * length;
+
+}
